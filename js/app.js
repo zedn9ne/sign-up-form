a18aef20c6b7d482c7018a44281ed333594d8e17
Mahdi Zanganeh <mahdizedn9ne@gmail.com>
2023-10-07 15:35:54 +0330

    app js finished

--- js/app.js
+++ js/app.js
@@ -1,9 +1,60 @@
-const form = document.querySelector('.form')
-const inputs = document.getElementsByTagName('input');
-const btn = document.querySelector('.btn');
-const value = inputs.value;
+const form = document.querySelector('.logIn');
+const fname = document.querySelector('.fname');
+const lname = document.querySelector('.lname');
+const email = document.querySelector('.email');
+const pass = document.querySelector('.pass');
+
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(value);
+    
+    var firstName = fname.value.trim();
+    var lastName = lname.value.trim();
+    var Email = email.value.trim();
+    var Password = pass.value.trim();
+    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (firstName === "") {
+        errorfunction(fname , "First name can not be empty")
+    } else {
+        success(fname);
+    }
+    if (lastName === "") {
+        errorfunction(lname , "Last name can not be empty")
+    } else {
+        success(lname);
+    }
+    if (Email === "") {
+        errorfunction(email , "Email can not be empty")
+    } else if (!Email.match(emailRegex)) {
+        
+        errorfunction(email , "Looks like not an email")
+    }
+    else {
+        success(email);
+    }
+    if (Password === "") {
+        errorfunction(pass , "Password can not be empty")
+    } else {
+        success(pass);
+    }
 })
+
+function errorfunction(req, message) {
+    var parentElement = req.parentElement;
+    var spanElement = parentElement.querySelector("span");
+    var inputElement = parentElement.querySelector('input');
+    inputElement.classList.add('alert');
+    spanElement.innerHTML = message;
+    if (req !== email) {
+        req.value = "";
+    }
+}
+function success(req) {
+    var parentElement = req.parentElement;
+    var inputElement = parentElement.querySelector('input');
+    var spanElement = parentElement.querySelector("span");
+    inputElement.classList.add('success');
+    inputElement.classList.remove('alert');
+    spanElement.innerHTML = "";
+}
